684da6551376232a103619c5f4dbc7e06dcb85f6
Mitra Kalloo <Mkalloo42@gmail.com>
2023-10-21 06:28:13 -0400

    Update utilities.js

--- src/utilities.js
+++ src/utilities.js
@@ -1,3 +1,8 @@
+
+// const Cost = [0.26, 0.32, 0.37];
+// const Levels = [400, 600, 999999];
+// calculateBill({cost: Cost, levels:Levels},2000,0,0)
+
 export const calculateBill = (config = {
     cost: [],
     levels: []
